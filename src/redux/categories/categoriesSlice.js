bf84b5d4bc949d6dcf7de0ebe4408c75144623ab
joyce <joyce.ann.cruz@gmail.com>
2023-08-23 23:15:45 +0800

    Configure store

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: {
+    categories: [],
+    status: "Under construction",
+  },
+  reducers: {
+    // other reducer actions if needed
+  },
+});
+
+export default categoriesSlice.reducer;
